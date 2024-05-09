85f85db51f4df870a3a4f9227e86314db2a719b2
root <phashateboho@gmail.com>
2024-05-09 05:26:31 -0700

    setup test database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user if does not exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
