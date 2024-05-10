64893bc9cf28f7c117af66eddfe35c3039d75ef2
Phxsha <phashateboho@gmail.com>
2024-05-10 16:58:17 +0200

    .

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
         if cls:
             return {
                 key: obj for key,
-                 obj in self.__objects.items() if isinstance(obj, cls)}
+                    obj in self.__objects.items() if isinstance(obj, cls)}
         return self.__objects
 
     def new(self, obj):
