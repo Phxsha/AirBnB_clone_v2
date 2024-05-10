a3d20e12705a0e72e933998f08d737f1c7db46b4
Phxsha <phashateboho@gmail.com>
2024-05-10 16:02:46 +0200

    changes to init

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,6 +17,11 @@ class DBStorage:
 
     def __init__(self):
         """Initialize DBStorage"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                        format(getenv('HBNB_MYSQL_USER'),
                                               getenv('HBNB_MYSQL_PWD'),
